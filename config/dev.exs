c6cd6d702e953af509357c4aa19678d2f1c8c521
Ilyes512 <ilyes.ahidar@gmail.com>
2016-08-23 20:54:50 +0200

    Prepared some config boilerplate for dev env only (for now)

---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# You can configure for your application as:
+#
+#     config :lightspeedex, key: :value
+
+import_config "dev.secret.exs"
---
+++ config/dev.secret.example.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+# You can configure for your application as:
+#
+#     config :lightspeedex, key: :value
+
